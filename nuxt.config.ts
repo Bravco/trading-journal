e6cc1a9ad63da3a56e5542019e51e079675ac098
Bravco <boss.wiw@gmail.com>
2024-07-05 11:46:02 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui"],
+
+  colorMode: {
+    preference: "dark",
+  },
+
+  ui: {
+    icons: ["ph"],
+  },
+
+  compatibilityDate: "2024-07-05",
+})
