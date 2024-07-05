aebe06f15960631e1ae7dbe104e8ed3cc4936b33
Bravco <boss.wiw@gmail.com>
2024-07-05 12:15:04 +0200

    Setup pages and default layout, trying out tailwindcss

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <UContainer class="flex justify-between gap-6 px-0 sm:px-0 lg:px-0">
+            <UCard v-for="_ in 4" class="w-full">
+                <template #header>
+                    <Placeholder class="h-6"/>
+                </template>
+                <Placeholder class="h-24"/>
+            </UCard>
+        </UContainer>
+    </div>
+</template>
