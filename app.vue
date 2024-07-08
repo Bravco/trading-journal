b7629fd13313263d7f03ac3f686b7930bbd51f3a
Bravco <boss.wiw@gmail.com>
2024-07-08 14:27:25 +0200

    Started creating the slideover for a new trade

--- app.vue
+++ app.vue
@@ -3,33 +3,88 @@
         <UContainer class="max-w-full py-4 lg:py-6 mb-0 sm:mb-4 bg-background/75 backdrop-blur border-b sticky top-0 z-50 border-gray-200 dark:border-gray-800">
             <div class="flex justify-between items-center">
                 <h1 class="text-xl font-bold">Trading <span class="text-primary">Journal</span></h1>
-                <UButton @click="" label="New Trade" icon="i-heroicons-document-plus" variant="solid"/>
+                <UButton @click="isSlideoverOpen = true" label="New Trade" icon="i-heroicons-document-plus" variant="solid"/>
             </div>
 
         </UContainer>
     </header>
-    
     <main>
         <NuxtLayout>
             <NuxtPage/>
         </NuxtLayout>
     </main>
-
     <footer>
         <UContainer class="max-w-full py-4 lg:py-6 mt-4 sm:mt-10 bg-background/75 backdrop-blur border-t border-gray-200 dark:border-gray-800 flex justify-center items-center text-sm">
-            <span>
-                Copyright &copy; {{ new Date().getFullYear() }} | Created by
-                <ULink 
-                    to="https://hamracek.dev" 
-                    target="_blank"
-                    class="underline hover:text-primary"
-                >Andrej Hamráček</ULink>
-            </span>
+            <span>Copyright &copy; 2024</span>
         </UContainer>
     </footer>
-
+    <USlideover v-model="isSlideoverOpen" :ui="{ width: 'max-w-xl' }">
+        <UCard class="h-full" :ui="{ rounded: 'rounded-none' }">
+            <template #header>
+                <div class="flex justify-between items-center">
+                    <div class="flex items-center gap-2">
+                        <UIcon name="i-heroicons-document-plus"/>
+                        <h1 class="text-lg font-medium">New Trade</h1>
+                    </div>
+                    <UButton @click="isSlideoverOpen = false" icon="i-heroicons-x-mark" variant="ghost" color="gray"/>
+                </div>
+            </template>
+            <UForm @submit="isSlideoverOpen = false" class="space-y-4">
+                <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                    <UFormGroup label="Symbol" name="symbol">
+                        <UInput type="text"/>
+                    </UFormGroup>
+                    <UFormGroup label="Open Date" name="openDate">
+                        <UInput type="date"/>
+                    </UFormGroup>
+                    <UFormGroup label="Open Time" name="openTime">
+                        <UInput type="time"/>
+                    </UFormGroup>
+                    <UFormGroup label="Risk Reward" name="rr">
+                        <UInput type="number" min="0"/>
+                    </UFormGroup>
+                    <UFormGroup label="Original Risk" name="risk">
+                        <UInput type="number" min="0"/>
+                    </UFormGroup>
+                    <UFormGroup label="Net P&L" name="pnl">
+                        <UInput type="number"/>
+                    </UFormGroup>
+                </div>
+                <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                    <UFormGroup label="Image Url" name="imageUrl">
+                        <UInput type="text"/>
+                    </UFormGroup>
+                    <UFormGroup label="Strategy" name="strategy">
+                        <UInput v-model="strategyInput" type="text">
+                            <template #trailing>
+                                <UDropdown :items="strategyOptions" class="pointer-events-auto">
+                                    <UIcon name="i-heroicons-chevron-down-20-solid "/>
+                                </UDropdown>
+                            </template>
+                        </UInput>
+                    </UFormGroup>
+                </div>
+                <UFormGroup label="Tags" name="tags">
+                    <UTextarea/>
+                </UFormGroup>
+                <UButton type="submit" label="Submit"/>
+            </UForm>
+        </UCard>
+    </USlideover>
     <UNotifications/>
 </template>
+
+<script lang="ts" setup>
+    const strategyOptions: any[] = [
+        strategies.map(strategy => ({
+            label: strategy,
+            click: () => strategyInput.value = strategy,
+        })),
+    ];
+
+    const isSlideoverOpen = ref<boolean>(false);
+    const strategyInput = ref<string | null>();
+</script>
 
 <style>
     :root {
--- pages/index.vue
+++ pages/index.vue
@@ -118,11 +118,6 @@
                         <UBadge v-if="row.pnl < 0" label="Lose" variant="subtle" color="red"/>
                         <UBadge v-if="row.pnl === 0" label="Breakeven" variant="subtle" color="blue"/>
                     </template>
-                    <template #imageUrl-data="{ row }">
-                        <ULink :to="row.imageUrl" target="_blank" class="cursor-pointer hover:opacity-50 transition">
-                            <NuxtImg :src="row.imageUrl" class="h-8"/>
-                        </ULink>
-                    </template>
                     <template #actions-data="{ row }">
                         <UDropdown :items="tradeActions(row)">
                             <UButton color="gray" variant="ghost" icon="i-heroicons-ellipsis-horizontal" square/>
@@ -209,100 +204,10 @@ const columns = [
     { key: "risk", label: "Original Risk", sortable: true },
     { key: "pnl", label: "Net P&L", sortable: true },
     { key: "status", label: "Status" },
-    { key: "imageUrl", label: "Image" },
     { key: "actions" },
 ];
 
-const tags: Tag[] = [
-    { label: "Liquidity Sweep", color: "orange" },
-    { label: "High Timeframe Fair Value Gap", color: "green" },
-    { label: "Fair Value Gap", color: "green" },
-    { label: "Inverse Fair Value Gap", color: "yellow" },
-    { label: "Break of Structure", color: "red" },
-];
-
-const trades = ref<Trade[]>([
-    {
-        open: new Date("2024-07-05T19:58:00"),
-        symbol: "YM",
-        strategy: "Liquidity Sweep + IFVG",
-        risk: 8.7,
-        pnl: 5.13,
-        rr: 1.17,
-        imageUrl: "https://www.tradingview.com/x/IqNKyxNQ/",
-        tags: [ tags[0] ],
-    },
-    {
-        open: new Date("2024-07-05T15:35:00"),
-        symbol: "NQ",
-        strategy: "Liquidity Sweep + FVG",
-        risk: 10,
-        pnl: 21,
-        rr: 2.1,
-        imageUrl: "https://www.tradingview.com/x/IqNKyxNQ/",
-        tags: tags,
-    },
-    {
-        open: new Date("2024-07-04T14:24:00"),
-        symbol: "GBPUSD",
-        strategy: "Demand + FVG",
-        risk: 10.49,
-        pnl: -7.64,
-        rr: 3.08,
-        imageUrl: "https://www.tradingview.com/x/IqNKyxNQ/",
-        tags: [ tags[3] ],
-    },
-    {
-        open: new Date("2024-07-03T15:11:00"),
-        symbol: "EURUSD",
-        strategy: "Demand + IFVG",
-        risk: 9.87,
-        pnl: -9.87,
-        rr: 1.93,
-        imageUrl: "https://www.tradingview.com/x/IqNKyxNQ/",
-        tags: [ tags[3] ],
-    },
-    {
-        open: new Date("2024-07-02T16:02:00"),
-        symbol: "NQ",
-        strategy: "Liquidity Sweep + FVG",
-        risk: 10.1,
-        pnl: 24.12,
-        rr: 3.29,
-        imageUrl: "https://www.tradingview.com/x/IqNKyxNQ/",
-        tags: [ tags[0] ],
-    },
-    {
-        open: new Date("2024-07-02T19:41:00"),
-        symbol: "NQ",
-        strategy: "Liquidity Sweep + FVG",
-        risk: 9.97,
-        pnl: 17.64,
-        rr: 2.03,
-        imageUrl: "https://www.tradingview.com/x/IqNKyxNQ/",
-        tags: [ tags[0] ],
-    },
-    {
-        open: new Date("2024-07-01T09:55:00"),
-        symbol: "NQ",
-        strategy: "Liquidity Sweep + IFVG",
-        risk: 10.54,
-        pnl: 20.94,
-        rr: 2.53,
-        imageUrl: "https://www.tradingview.com/x/IqNKyxNQ/",
-        tags: [ tags[0] ],
-    },
-    {
-        open: new Date("2024-06-28T20:45:00"),
-        symbol: "YM",
-        strategy: "Liquidity Sweep + FVG",
-        risk: 9.15,
-        pnl: 0,
-        rr: 1.94,
-        imageUrl: "https://www.tradingview.com/x/IqNKyxNQ/",
-        tags: [ tags[0] ],
-    },
-]);
+const trades = useTrades();
 
 const tradeActions = (trade : Trade) => [
     [
